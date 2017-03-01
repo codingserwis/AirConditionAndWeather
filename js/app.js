79e2f461b72ec8cde0b975b0b6cbd3737c4d7e12
Maciej Lehrer <maciej.lehrer@gmail.com>
2017-03-01 11:51:51 +0100

    Zmiany w wyglądzie wyświetlanych danych. Poprawka kodu js do wyświetlania danych IJP. Rozpoczęte prace nad aktualną pogodą.

--- js/app.js
+++ js/app.js
@@ -1,261 +1,249 @@
-//*** KEY TO LOOKO2 API - 1487184056
-	var apiKey = '1487184056';
-	
+// *** KEY TO LOOKO2 API - 1487184056 ***
+// *** KEY TO APIXU WEATHER - 48147044f89b4001831152133171402 ***
+// *** BASIC URL TO API - http://api.looko2.com/?method=GetLOOKO&id=${stationID}&token=${apiKey}
+var lookoApiKey = '1487184056',
+	chabryId = '5ccf7fc18200',
+	pulaId = '5ccf7fc255ee',
+	pilId = '6001940094f8',
+	pasId = '5ccf7fc17d7d',
+	nwkId = '5ccf7fc18052',
+	gruId = '60019400a82b',
+	apixuKey = '48147044f89b4001831152133171402'
+	apixuUrl = 'http://api.apixu.com/v1/current.json?key=48147044f89b4001831152133171402&q=Opole';
+
+
+// *** Połączenie z Api OSA Chabry ***	
+	function connectionToApiChabry(){
+		$.ajax({
+			url: `http://api.looko2.com/?method=GetLOOKO&id=${chabryId}&token=${lookoApiKey}`,
+			dataType: 'json',
+			type: 'GET'
+		}).done(function(response){
+			insertData(response, '#chab');
+		}).fail(function(error){
+			console.log(error);
+		})
+	}
+
+// *** Połączenie z Api OSA Pulaskego ***
+	function connectionToApiPulaskiego(){
+		$.ajax({
+			url: `http://api.looko2.com/?method=GetLOOKO&id=${pulaId}&token=${lookoApiKey}`,
+			dataType: 'json',
+			type: 'GET'
+		}).done(function(response){
+			insertData(response, '#pul');
+		}).fail(function(error){
+			console.log(error);
+		})
+	}
+
+// *** Połączenie z Api OSA Rondo Pileckiego ***
+	function connectionToApiPileckego(){
+		$.ajax({
+			url: `http://api.looko2.com/?method=GetLOOKO&id=${pilId}&token=${lookoApiKey}`,
+			dataType: 'json',
+			type: 'GET'
+		}).done(function(response){
+			insertData(response, '#pil');
+		}).fail(function(error){
+			console.log(error);
+		})
+	}
+
+// *** Połączenie z Api OSA Pasieka ***
+	function connectionToApiPasieka(){
+		$.ajax({
+			url: `http://api.looko2.com/?method=GetLOOKO&id=${pasId}&token=${lookoApiKey}`,
+			dataType: 'json',
+			type: 'GET'
+		}).done(function(response){
+			insertData(response, '#pas');
+		}).fail(function(error){
+			console.log(error);
+		})
+	}
+
+// *** Połączenie z Api OSA Nowa Wieś Królewska ***
+	function connectionToApiNwk(){
+		$.ajax({
+			url: `http://api.looko2.com/?method=GetLOOKO&id=${nwkId}&token=${lookoApiKey}`,
+			dataType: 'json',
+			type: 'GET'
+		}).done(function(response){
+			insertData(response, '#nwk');
+		}).fail(function(error){
+			console.log(error);
+		})
+	}
+
+// *** Połączenie z Api OSA Grudzice ***
+	function connectionToApiGrudzice(){
+		$.ajax({
+			url: `http://api.looko2.com/?method=GetLOOKO&id=${gruId}&token=${lookoApiKey}`,
+			dataType: 'json',
+			type: 'GET'
+		}).done(function(response){
+			insertData(response, '#gru');
+		}).fail(function(error){
+			console.log(error);
+		})
+	}
+
+// *** Połączenie do APIXU API ***
+	function connectionToApixu(){
+		$.ajax({
+			url: apixuUrl,
+			dataType: 'json',
+			type: 'GET'
+		}).done(function(response){
+			console.log(response);
+			insertWeatherData(response);
+		}).fail(function(error){
+			console.log(error);
+		})
+	}
+// *** Sprawdzanie warunków pogodowych ***
+	function checkWeatherCondition(response){
+		if (response.current.is_day === 1){
+			if(response.current.condition.code === 1000){
+				return '<i class="wi wi-day-sunny"></i>';
+			} else if(response.current.condition.code === 1003){
+				return '<i class="wi wi-day-cloudy"></i>';
+			}
+		} else if(response.current.is_day === 0){
+			if(response.current.condition.code === 1000){
+				return '<i class="wi wi-night-clear"></i>';
+			} else if(response.current.condition.code === 1003){
+				return '<i class="wi wi-night-alt-cloudy"></i>';
+			}
+		}	
+	}
+
+// *** Sprawdzanie kierunku wiatru ***
+	function checkWindDir(response){
+		if(response.current.wind_dir == 'SSW'){
+			return '<i class="wi wi-wind wi-from-ssw"></i>';
+		} else if(response.current.wind_dir == 'SW'){
+			return '<i class="wi wi-wind wi-from-sw"></i>';
+		}
+	}
+// *** Dane pogodowe ***
+	function insertWeatherData(response){
+		var wCondition = $('.w_condition'),
+			wTemp = $('.w_temp'),
+			wWindSpeed = $('.w_wind_speed'),
+			wWindDir = $('.w_wind_dir'),
+			wPresure = $('.w_presure'),
+			wPrecipMm = $('.w_precip_mm'),
+			wHumidity = $('.w_humidity')
+			wFeels = $('.w_feels'),
+			wVis = $('.w_vis');
+
+			wCondition.html(checkWeatherCondition(response));
+			wWindDir.html(checkWindDir(response));
+			wTemp.html(`<p>${response.current.temp_c}&ordmC</p>`);
+			wWindSpeed.html(`<span>${response.current.wind_kph} km/h</span>`);
+			wPresure.html(`<span>${response.current.pressure_mb} mb</span>`);
+			wPrecipMm.html(`<span>${response.current.precip_mm} mm</span>`);
+			wHumidity.html(`<span>${response.current.humidity} %</span>`);
+			wFeels.html(`<span>${response.current.feelslike_c}&ordmC</span>`);
+			wVis.html(`<span>${response.current.vis_km} km</span>`);
+	}
 // *** Sprawdzanie poziomu IJP
 	function checkValueOfIjp(paramName, elemId){
 			if(paramName >= 0 && paramName <= 1){
-				elemId.css('background-color', 'blue');
+				elemId.css('background-color', '#1b5e20');
 			} else if(paramName >= 2 && paramName <= 3){
-				elemId.css('background-color', 'green');
+				elemId.css('background-color', '#8bc34a');
 			} else if(paramName >= 4 && paramName <= 5){
-				elemId.css('background-color', 'yellow');
+				elemId.css('background-color', '#fdd835');
 			} else if(paramName >= 6 && paramName <= 7){
-				elemId.css('background-color', 'orange');
+				elemId.css('background-color', '#bf360c');
 			} else if(paramName >= 8 && paramName <= 9){
-				elemId.css('background-color', 'red');
+				elemId.css('background-color', '#b71c1c');
 			} else if(paramName >= 10){
-				elemId.css('background-color', 'brown');
+				elemId.css('background-color', '#000');
 			}
 	}
 	function checkValueOfPm2(paramName, elemId){
 		if(paramName >= 0 && paramName <= 12){
-				elemId.css('background-color', 'blue');
+				elemId.css('background-color', '#1b5e20');
 			} else if(paramName >= 13 && paramName <= 36){
-				elemId.css('background-color', 'green');
+				elemId.css('background-color', '#8bc34a');
 			} else if(paramName >= 37 && paramName <= 60){
-				elemId.css('background-color', 'yellow');
+				elemId.css('background-color', '#fdd835');
 			} else if(paramName >= 61 && paramName <= 84){
-				elemId.css('background-color', 'orange');
+				elemId.css('background-color', '#bf360c');
 			} else if(paramName >= 85 && paramName <= 120){
-				elemId.css('background-color', 'red');
+				elemId.css('background-color', '#b71c1c');
 			} else if(paramName > 121){
-				elemId.css('background-color', 'brown');
+				elemId.css('background-color', '#000');
 			}
 	}		
 	function checkValueOfPm10(paramName, elemId){
 		if(paramName >= 0 && paramName <= 20){
-				elemId.css('background-color', 'blue');
+				elemId.css('background-color', '#1b5e20');
 			} else if(paramName >= 21 && paramName <= 60){
-				elemId.css('background-color', 'green');
+				elemId.css('background-color', '#8bc34a');
 			} else if(paramName >= 61 && paramName <= 100){
-				elemId.css('background-color', 'yellow');
+				elemId.css('background-color', '#fdd835');
 			} else if(paramName >= 101 && paramName <= 140){
-				elemId.css('background-color', 'orange');
+				elemId.css('background-color', '#bf360c');
 			} else if(paramName >= 141 && paramName <= 200){
-				elemId.css('background-color', 'red');
+				elemId.css('background-color', '#b71c1c');
 			} else if(paramName > 201){
-				elemId.css('background-color', 'brown');
+				elemId.css('background-color', '#000');
 			}
-	}		
-// *** Połączenie z Api OSA Chabry ***	
-	function connectionToApiChabry(){
-		$.ajax({
-			url: `http://api.looko2.com/?method=GetLOOKO&id=5ccf7fc18200&token=${apiKey}`,
-			dataType: 'json',
-			type: 'GET'
-		}).done(function(response){
-			chabryInfo(response);
-		}).fail(function(error){
-			console.log(error);
-		})
-	}
-// *** Połączenie z Api OSA Puławskiego ***
-	function connectionToApiPulaskiego(){
-		$.ajax({
-			url: `http://api.looko2.com/?method=GetLOOKO&id=5ccf7fc255ee&token=${apiKey}`,
-			dataType: 'json',
-			type: 'GET'
-		}).done(function(response){
-			pulaskiegoInfo(response);
-		}).fail(function(error){
-			console.log(error);
-		})
-	}
-// *** Połączenie z Api OSA Pasieka ***
-	function connectionToApiPasieka(){
-		$.ajax({
-			url: `http://api.looko2.com/?method=GetLOOKO&id=5ccf7fc17d7d&token=${apiKey}`,
-			dataType: 'json',
-			type: 'GET'
-		}).done(function(response){
-			pasiekaInfo(response);
-		}).fail(function(error){
-			console.log(error);
-		})
-	}
-// *** Połączenie z Api OSA Nowa Wieś Królewska ***
-	function connectionToApiNwk(){
-		$.ajax({
-			url: `http://api.looko2.com/?method=GetLOOKO&id=5ccf7fc18052&token=${apiKey}`,
-			dataType: 'json',
-			type: 'GET'
-		}).done(function(response){
-			NwkInfo(response);
-		}).fail(function(error){
-			console.log(error);
-		})
-	}
-// *** Połączenie z Api OSA Grudzice ***
-	function connectionToApiGrudzice(){
-		$.ajax({
-			url: `http://api.looko2.com/?method=GetLOOKO&id=60019400a82b&token=${apiKey}`,
-			dataType: 'json',
-			type: 'GET'
-		}).done(function(response){
-			grudziceInfo(response);
-		}).fail(function(error){
-			console.log(error);
-		})
-	}
-// *** Połączenie z Api OSA Pileckiego ***
-	function connectionToApiPileckiego(){
-		$.ajax({
-			url: `http://api.looko2.com/?method=GetLOOKO&id=6001940094f8&token=${apiKey}`,
-			dataType: 'json',
-			type: 'GET'
-		}).done(function(response){
-			pileckiegoInfo(response);
-		}).fail(function(error){
-			console.log(error);
-		})
-	}
-// *** Małe Info Chabry
-	function chabryInfo(response){
-		var ijp = $('#ch_ijp'),
-			bigIjp = $('#cha_big_d_ijp'),
-			bigPm2 = $('#big_cha_pm2'),
-			bigPm2Box = $('#cha_big_d_pm2');
-			bigPm10 = $('#big_cha_pm10'),
-			pm01Box = $('#ch_pm1'), 
-			bigIjpBox = $('#big_cha_ijp'),
-			pm25Box = $('#ch_pm2'),
-			pm10Box = $('#ch_pm10'),
-			ijpVal = $('#ch_ijp_val'),
-			pm01 = $('#ch_pm1_val'),
-			pm25 = $('#ch_pm2_val'),
-			pm10 = $('#ch_pm10_val');
-			ijpVal.text(response.IJP);
-			pm01.text(response.PM1);
-			bigIjpBox.text(response.IJP);
-			bigPm2.text(response.PM25);
-			bigPm10.text(response.PM10);
-			pm25.text(response.PM25);
-			pm10.text(response.PM10);
-			checkValueOfIjp(response.IJP, ijp);
-			checkValueOfPm2(response.PM25, pm25Box);
-			checkValueOfPm10(response.PM10, pm10Box);
-	}
-// *** Małe info Pułaskiego ***
-	function pulaskiegoInfo(response){
-		var ijp = $('#p_ijp'),
-			pm01Box = $('#p_pm1'),
-			pm25Box = $('#p_pm2'),
-			pm10Box = $('#p_pm10'),
-			ijpVal = $('#p_ijp_val'),
-			pm01 = $('#p_pm1_val'),
-			pm25 = $('#p_pm2_val'),
-			pm10 = $('#p_pm10_val');
-			ijpVal.text(response.IJP);
-			pm01.text(response.PM1);
-			pm25.text(response.PM25);
-			pm10.text(response.PM10);
-			checkValueOfIjp(response.IJP, ijp);
-			checkValueOfPm2(response.PM25, pm25Box);
-			checkValueOfPm10(response.PM10, pm10Box);
-	}
-// *** Małe info Pasieka ***
-	function pasiekaInfo(response){
-		var ijp = $('#pa_ijp'),
-			pm01Box = $('#pa_pm1'),
-			pm25Box = $('#pa_pm2'),
-			pm10Box = $('#pa_pm10'),
-			ijpVal = $('#pa_ijp_val'),
-			pm01 = $('#pa_pm1_val'),
-			pm25 = $('#pa_pm2_val'),
-			pm10 = $('#pa_pm10_val');
-			ijpVal.text(response.IJP);
-			pm01.text(response.PM1);
-			pm25.text(response.PM25);
-			pm10.text(response.PM10);
-			checkValueOfIjp(response.IJP, ijp);
-			checkValueOfPm2(response.PM25, pm25Box);
-			checkValueOfPm10(response.PM10, pm10Box);
-	}
-// *** Małe info Nowa Wieś Królewska ***
-	function NwkInfo(response){
-		var ijp = $('#nw_ijp'),
-			pm01Box = $('#nw_pm1'),
-			pm25Box = $('#nw_pm2'),
-			pm10Box = $('#nw_pm10'),
-			ijpVal = $('#nw_ijp_val'),
-			pm01 = $('#nw_pm1_val'),
-			pm25 = $('#nw_pm2_val'),
-			pm10 = $('#nw_pm10_val');
-			ijpVal.text(response.IJP);
-			pm01.text(response.PM1);
-			pm25.text(response.PM25);
-			pm10.text(response.PM10);
-			checkValueOfIjp(response.IJP, ijp);
-			checkValueOfPm2(response.PM25, pm25Box);
-			checkValueOfPm10(response.PM10, pm10Box);
-	}
-// *** Małe info Grudzice ***
-	function grudziceInfo(response){
-		var ijp = $('#g_ijp'),
-			pm01Box = $('#g_pm1'),
-			pm25Box = $('#g_pm2'),
-			pm10Box = $('#g_pm10'),
-			ijpVal = $('#g_ijp_val'),
-			pm01 = $('#g_pm1_val'),
-			pm25 = $('#g_pm2_val'),
-			pm10 = $('#g_pm10_val');
-			ijpVal.text(response.IJP);
-			pm01.text(response.PM1);
-			pm25.text(response.PM25);
-			pm10.text(response.PM10);
-			checkValueOfIjp(response.IJP, ijp);
-			checkValueOfPm2(response.PM25, pm25Box);
-			checkValueOfPm10(response.PM10, pm10Box);
-	}
-// *** Małe info Pileckiego ***
-	function pileckiegoInfo(response){
-		var ijp = $('#pi_ijp'),
-			pm01Box = $('#pi_pm1'),
-			pm25Box = $('#pi_pm2'),
-			pm10Box = $('#pi_pm10'),
-			ijpVal = $('#pi_ijp_val'),
-			pm01 = $('#pi_pm1_val'),
-			pm25 = $('#pi_pm2_val'),
-			pm10 = $('#pi_pm10_val');
-			ijpVal.text(response.IJP);
-			pm01.text(response.PM1);
-			pm25.text(response.PM25);
-			pm10.text(response.PM10);
-			checkValueOfIjp(response.IJP, ijp);
-			checkValueOfPm2(response.PM25, pm25Box);
-			checkValueOfPm10(response.PM10, pm10Box);
-	}
+	}	
 
-// *** Fubkcja otwierająca dane szczegółowe z danego urządzenia
-function bigWindowInfo(){
-	var stationInfo = $('#more_info'),
-		bigStationInfo = $('.big_data_container');
-		stationInfo.on('click', function(){
-			bigStationInfo.addClass('slide_right');
-		}); 
-}
+// *** Wstawianie danych ***
+	function insertData(response, stId){
+		var stationIjp = $(`${stId}`).find('.ijp_data'),
+			stationPm01 = $(`${stId}`).find('.pm01_data'),
+			stationPm25 = $(`${stId}`).find('.pm25_data'),
+			stationPm10 = $(`${stId}`).find('.pm10_data');
+
+			stationIjp.html(`<h3>IJP</h3><p class="ijp">${response.IJP}</p>`);
+			stationPm01.html(`<h3>PM 0.1</h3><p>${response.PM1}</p><p class="units">&microg/m<sup>3</sup></p>`);
+			stationPm25.html(`<h3>PM 2.5</h3><p>${response.PM25}</p><p class="units">&microg/m<sup>3</sup></p>`);
+			stationPm10.html(`<h3>PM 10</h3><p>${response.PM10}</p><p class="units">&microg/m<sup>3</sup></p>`);
+
+			checkValueOfIjp(response.IJP, stationIjp);
+			checkValueOfPm2(response.PM25, stationPm25);
+			checkValueOfPm10(response.PM10, stationPm10);
+
+	}
+// *** Akcje na doomie ***
+// *** Zwijanie dóżego info ***
+	function hideBigData(){
+		var hideBtn = $('.slide_up'),
+			bigContainer = $('.big_data_container');
+
+			hideBtn.on('click', function(){
+				bigContainer.animate({height: 0, opacity: 0}, 1000);
+			});
+	}
+// *** Document listner ***
 $(document).ready(function(){
-connectionToApiChabry();
-connectionToApiPulaskiego();
-connectionToApiPasieka();
-connectionToApiNwk();
-connectionToApiGrudzice();
-connectionToApiPileckiego();
-bigWindowInfo();
+	// *** Wywołanie funkcji ***
+	connectionToApiChabry();
+	connectionToApiPulaskiego();
+	connectionToApiPileckego();
+	connectionToApiPasieka();
+	connectionToApiNwk();
+	connectionToApiGrudzice();
+	connectionToApixu()
+	//hideBigData();
 });
-
 // *** Mapa Google ***
-var chabryStr = {center: {lat: 50.6791, lng: 17.9265}},
+var chabryStr = {
+		center: {
+			lat: 50.6791, 
+			lng: 17.9265
+		},
+		color: 'blue'
+	},
 	pulawskiegoStr = {center: {lat: 50.6733, lng: 17.925}},
 	pasiekaStr = {center: {lat: 50.6619, lng: 17.9201}},
 	nowaWkStr = {center: {lat: 50.6533, lng: 17.9521}},
@@ -267,7 +255,8 @@ function initMap() {
     var uluru = {lat: 50.6705469, lng: 17.8830356};
     var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
-    center: uluru
+    center: uluru,
+    disableDefaultUI: true,
     });
 
 // *** Punktuy pomiarowe na mapie ***
@@ -275,12 +264,12 @@ function initMap() {
     		strokeColor: '#FF0000',
             strokeOpacity: 0.8,
             strokeWeight: 1,
-            fillColor: 'red',
+            fillColor: chabryStr.color,//'red',
             fillOpacity: 0.35,
             map: map,
             center: chabryStr.center,
             radius: 400
-        });
+       });
         var pulaskiegoCircle = new google.maps.Circle({
     		strokeColor: '#FF0000',
             strokeOpacity: 0.8,
@@ -332,40 +321,22 @@ function initMap() {
             radius: 400
         });
 
-    chabryCircle.addListener('mouseover', function() {
-          $('#s_chab').addClass('bigger');
+    function markedCircle(circle, elemToMark ){
+    	var dataToMark = $(`${elemToMark}`).parent();
+    	var bigContainer = $('.big_data_container');
+    	circle.addListener('mouseover', function() {
+          dataToMark.css('border', '2px solid red');
+          bigContainer.animate({opacity: 1, height: '90vh'}, 1000);
+
         });
-    chabryCircle.addListener('mouseout', function() {
-          $('#s_chab').removeClass('bigger');
+    	circle.addListener('mouseout', function() {
+          dataToMark.css('border', '2px solid transparent');
         });
-    pulaskiegoCircle.addListener('mouseover', function() {
-          $('#s_pul').addClass('bigger');
-        });
-    pulaskiegoCircle.addListener('mouseout', function() {
-          $('#s_pul').removeClass('bigger');
-        });
-    pasiekaCircle.addListener('mouseover', function() {
-          $('#s_pas').addClass('bigger');
-        });
-    pasiekaCircle.addListener('mouseout', function() {
-          $('#s_pas').removeClass('bigger');
-        });
-    nwkCircle.addListener('mouseover', function() {
-          $('#s_nwk').addClass('bigger');
-        });
-    nwkCircle.addListener('mouseout', function() {
-          $('#s_nwk').removeClass('bigger');
-        });
-    grudziceCircle.addListener('mouseover', function() {
-          $('#s_gru').addClass('bigger');
-        });
-    grudziceCircle.addListener('mouseout', function() {
-          $('#s_gru').removeClass('bigger');
-        });
-    strzeleckaCircle.addListener('mouseover', function() {
-          $('#s_pil').addClass('bigger');
-        });
-    strzeleckaCircle.addListener('mouseout', function() {
-          $('#s_pil').removeClass('bigger');
-        });
+    }
+    markedCircle(chabryCircle, '#chab');
+    markedCircle(pulaskiegoCircle, '#pul');
+    markedCircle(pasiekaCircle, '#pas');
+    markedCircle(nwkCircle, '#nwk');
+    markedCircle(grudziceCircle, '#gru');
+    markedCircle(strzeleckaCircle, '#pil');
 }
