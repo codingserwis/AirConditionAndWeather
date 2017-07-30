3609c47426f7a60c06c8b57f6e9fc6bf8803e717
Maciej Lehrer <maciej.lehrer@gmail.com>
2017-07-30 21:45:37 +0200

    added value checking functions

--- prod/js/app.js
+++ prod/js/app.js
@@ -42,14 +42,15 @@ const IJPApi = (()=> {
 	};
 	// insert data to DOM
 	const insertIJPData = (data)=> {
-		let html, stationName, ijpBcgColor, pm25BcgColor;
+		let html, stationName, ijpBcgColor, pm25BcgColor, pm10BcgColor;
 			// getting the coect nam of station
 			stationName = nameChange(data);
 			ijpBcgColor = checkValueOfIJP(data);
 			pm25BcgColor = checkValueOfPM25(data);
+			pm10BcgColor = checkValueOfPM10(data);
 
 			// insertin data to the DOM
-			html = `<div class="station__container"><h2 class="station__container-header">${stationName}</h2><div class="station__container-data flex flex__row flex__justify-between"><div class="station__data-ijp ${ijpBcgColor}"><h3>IJP</h3><p class="data__ijp">${data.IJP}</p><p class="text__ijp">${data.IJPString}</p></div><div class="station__data-pm2 ${pm25BcgColor}"><h3>PM 2.5</h3><p class="data__pm2">${data.PM25}</p><p class="units">&microg/m<sup>3</sup></p></div><div class="station__data-pm1"><h3>PM 10</h3><p class="data__pm1">${data.PM10}</p><p class="units">&microg/m<sup>3</sup></p></div></div></div>`;
+			html = `<div class="station__container"><h2 class="station__container-header">${stationName}</h2><div class="station__container-data flex flex__row flex__justify-between"><div class="station__data-ijp ${ijpBcgColor}"><h3>IJP</h3><p class="data__ijp">${data.IJP}</p><p class="text__ijp">${data.IJPString}</p></div><div class="station__data-pm2 ${pm25BcgColor}"><h3>PM 2.5</h3><p class="data__pm2">${data.PM25}</p><p class="units">&microg/m<sup>3</sup></p></div><div class="station__data-pm1 ${pm10BcgColor}"><h3>PM 10</h3><p class="data__pm1">${data.PM10}</p><p class="units">&microg/m<sup>3</sup></p></div></div></div>`;
 			document.querySelector('.data__container-data').insertAdjacentHTML('beforeend', html);			
 	};
 	
@@ -116,6 +117,24 @@ const IJPApi = (()=> {
 	};
 
 	// setting the bcg color of PM10 container
+	const checkValueOfPM10 = (data)=> {
+		let pm10Bcg;
+
+		if (data.PM10 >= 0 && data.PM10 <= 20) {
+			pm10Bcg = 'data__bcg-dgreen';
+		} else if (data.PM10 >= 21 && data.PM10 <= 60) {
+			pm10Bcg = 'data__bcg-green';
+		} else if (data.PM10 >= 61 && data.PM10 <= 100) {
+			pm10Bcg = 'data__bcg-yellow';
+		} else if (data.PM10 >= 101 && data.PM10 <= 140) {
+			pm10Bcg = 'data__bcg-orange';
+		} else if (data.PM10 >= 141 && data.PM10 <= 200) {
+			pm25Bcg = 'data__bcg-red';
+		} else if (data.PM10 > 201) {
+			pm25Bcg = 'data__bcg-black';
+		}
+		return pm10Bcg;
+	};
 //	const checkValueOfPM10 = (data)=> {
 //		let 
 	// 	let pm10Container = document.querySelectorAll(appOptions.domStrings.pm10Data);
